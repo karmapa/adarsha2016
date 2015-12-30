395dfa27505c7ee904732edf5ac142f9c92560cb
km001 <kuang@utrust.com.tw>
2015-12-30 14:33:57 +0800

    setting.js

---
+++ setting.js
@@ -0,0 +1,5 @@
+//for server
+window.image_url_prefix="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/";
+
+//for local/USB
+window.image_url_prefix="../adarsha_img/lijiang/";
