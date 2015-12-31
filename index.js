f2eb17fbf1635dc097d62d1834afb489bffaf18a
yapcheahshen <yapcheahshen@gmail.com>
2015-12-31 10:08:29 +0800

    remove webruntime, ksanagap is not required

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 if (typeof window!=="undefined"){
-    runtime=require("ksana2015-webruntime");
-    runtime.boot("adarsha2016",function(){
+    window.kde=require("ksana-database");
+    window.kse=require("ksana-search");
+    window.ksa=require("ksana-simple-api");
 
-        window.kde=require("ksana-database");
-        window.kse=require("ksana-search");
-        window.ksa=require("ksana-simple-api");
+    window.React=require("react");
+    window.ReactDOM=require("react-dom");
+    window.ksana2015treetoc=require("ksana2015-treetoc");
+    window.ksana2015breadcrumbtoc=require("ksana2015-breadcrumbtoc");
+    window.wylie=require("tibetan/wylie");        
 
-        window.React=require("react");
-        window.ReactDOM=require("react-dom");
-        window.ksana2015treetoc=require("ksana2015-treetoc");
-        window.ksana2015breadcrumbtoc=require("ksana2015-breadcrumbtoc");
-        window.wylie=require("tibetan/wylie");        
-
-        systemReady();
-        systemReady2();
-    });
-
+    systemReady();
+    systemReady2();
 } else if (typeof module!=="undefined") {
     module.exports={kse:require("ksana-search"),kde:require("ksana-database"),ksa:require("ksana-simple-api")}
 }
