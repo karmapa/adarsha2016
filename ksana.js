93a98f8be2283ee5545ecdecf06058735953ffb6
yapcheahshen <yapcheahshen@gmail.com>
2016-01-04 16:22:47 +0800

    ignore NativeModules for compatible with ksana-jsonrom@1.5.4

--- ksana.js
+++ ksana.js
@@ -1,24 +1,29 @@
 jsonp_handler({
  "version": "1",
- "build": 280,
+ "build": 308,
  "title": "Adarsha",
  "minruntime": "1.2",
- "date": "2015-12-30T08:10:04.938Z",
+ "date": "2016-01-04T08:21:54.831Z",
  "description": "adarsha 2016",
  "baseurl": "http://github.com/karmapa/adarsha/",
+ "browserify": {
+  "external": [
+   "NativeModules"
+  ]
+ },
  "files": [
   "index.html",
   "bundle.js",
   "ksana.js"
  ],
  "filedates": [
-  "2015-12-30T07:59:18.992Z",
-  "2015-12-30T08:10:04.071Z",
-  "2015-12-30T08:07:18.387Z"
+  "2015-12-30T08:11:55.016Z",
+  "2016-01-04T08:21:53.940Z",
+  "2016-01-04T08:21:47.991Z"
  ],
  "filesizes": [
   25847,
-  977980,
-  425
+  931787,
+  485
  ]
 })
