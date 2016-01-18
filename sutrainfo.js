f8c5ec52368881513d9a7d52542048ce0d380b34
km001 <kuang@utrust.com.tw>
2016-01-18 17:25:33 +0800

    show sutra info update

--- sutrainfo.js
+++ sutrainfo.js
@@ -3,7 +3,13 @@ var fieldNames={
 }
 
 var getInfo=function(sutraid){
-
+    for(var i=0;i<biography.divisions.length;i++){
+        console.log("i:"+i);
+        for(var j=0;j<biography.divisions[i].sutras.length;j++){
+            if(biography.divisions[i].sutras[j].sutraid==sutraid)return biography.divisions[i].sutras[j];
+        }
+    }
+    return "";
 }
 
 var showSutraInfo=function(){
@@ -11,7 +17,8 @@ var showSutraInfo=function(){
     //根據經號 抓sutra info
     var out = "";
     out = "<table class=\"table table-condensed\">";
-    var sutrainfo=biography.divisions[0].sutras[0];
+    //var sutrainfo=biography.divisions[0].sutras[0];
+    var sutrainfo = getInfo(document.getElementById('sutraid').innerHTML);
     for(var i in sutrainfo){
         if(sutrainfo[i]){
             out += "<tr><th>"+(fieldNames[i]||i)+"</th><td>"+sutrainfo[i]+"</td></tr>";
