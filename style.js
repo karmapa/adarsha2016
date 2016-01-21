ed9149056488c1d0ed89614fb91af84fb001a2ab
km001 <kuang@utrust.com.tw>
2016-01-21 17:19:32 +0800

    font change is ratio now

--- style.js
+++ style.js
@@ -1,11 +1,11 @@
 var fontSize=parseInt(localStorage.getItem("fontsize")||17);
-var lineheight=parseInt(localStorage.getItem("lineheight")||35);
+var lineheight=parseInt(localStorage.getItem("lineheight")||2);
 var sheet=document.getElementsByTagName("STYLE")[0].sheet;
 
 var setFont=function(sChange,hChange){
     fontSize+=sChange;
-    lineheight+=hChange;
-    sheet.insertRule(".head-content p {line-height:"+lineheight+"pt;font-size:"+fontSize+"pt}",sheet.cssRules.length);
+    if((lineheight+hChange)<=3.5&&(lineheight+hChange)>=1.5)lineheight+=hChange;
+    sheet.insertRule(".head-content p {line-height:"+(fontSize*lineheight)+"pt;font-size:"+fontSize+"pt}",sheet.cssRules.length);
     localStorage.setItem("fontsize",fontSize);
     localStorage.setItem("lineheight",lineheight);
 }
