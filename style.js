0b7e440faf02b0a80aacde3160fb84022dc1b1f6
km001 <kuang@utrust.com.tw>
2016-01-25 10:21:53 +0800

    font breakproof

--- style.js
+++ style.js
@@ -4,6 +4,8 @@ var sheet=document.getElementsByTagName("STYLE")[0].sheet;
 
 var setFont=function(sChange,hChange){
     fontSize+=sChange;
+    if(lineheight>3.5)lineheight=3.5;
+    if(lineheight<1.5)lineheight=1.5;
     if((lineheight+hChange)<=3.5&&(lineheight+hChange)>=1.5)lineheight+=hChange;
     sheet.insertRule(".head-content p {line-height:"+(fontSize*lineheight)+"pt;font-size:"+fontSize+"pt}",sheet.cssRules.length);
     localStorage.setItem("fontsize",fontSize);
